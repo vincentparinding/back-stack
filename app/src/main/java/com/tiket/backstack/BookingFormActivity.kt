965cbaf8527dbef6fd853e39d1f6f4b8e68e2a43
vincentparinding <vincent.parinding@tiket.com>
2023-06-09 12:36:47 +0800

    use TaskStackBuilder

--- app/src/main/java/com/tiket/backstack/BookingFormActivity.kt
+++ app/src/main/java/com/tiket/backstack/BookingFormActivity.kt
@@ -1,11 +1,13 @@
 package com.tiket.backstack
 
-import android.app.ActivityManager
-import android.content.Context
+import android.app.PendingIntent
+import android.app.PendingIntent.CanceledException
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.app.TaskStackBuilder
+
 
 class BookingFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +15,32 @@ class BookingFormActivity : AppCompatActivity() {
         setContentView(R.layout.activity_booking_form)
 
         findViewById<AppCompatButton>(R.id.btn_back).setOnClickListener {
-            startActivity(Intent(this, PDPActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-                intent.putExtra("goto", "PDP 1")
-            })
+            backToPDP1()
+        }
+    }
+
+    private fun backToPDP1() {
+        val targetInstanceIdentifier = "PDPActivity_Instance1"
+        val srp1InstanceIdentifier = "SRPActivity_Instance1"
+
+        val stackBuilder: TaskStackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addNextIntentWithParentStack(Intent(this, MainActivity::class.java))
+
+        val targetIntent = Intent(this, PDPActivity::class.java)
+        targetIntent.putExtra("INSTANCE_IDENTIFIER", targetInstanceIdentifier)
+
+        val srp1 = Intent(this, SRPActivity::class.java)
+        srp1.putExtra("INSTANCE_IDENTIFIER", srp1InstanceIdentifier)
+
+        stackBuilder.addNextIntent(srp1)
+        stackBuilder.addNextIntent(targetIntent)
+
+        val pendingIntent: PendingIntent? =
+            stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        try {
+            pendingIntent?.send()
+        } catch (e: CanceledException) {
+            e.printStackTrace()
         }
     }
 }
--- app/src/main/java/com/tiket/backstack/PDPActivity.kt
+++ app/src/main/java/com/tiket/backstack/PDPActivity.kt
@@ -1,9 +1,9 @@
 package com.tiket.backstack
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
 class PDPActivity : AppCompatActivity() {
@@ -13,29 +13,17 @@ class PDPActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdpactivity)
 
-        localIndex = "PDP ${Index.index+1}"
+        localIndex = "PDP ${Index.index + 1}"
         findViewById<TextView>(R.id.tv_pdp).text = localIndex
         findViewById<AppCompatButton>(R.id.btn_book).setOnClickListener {
-            if (Index.index == 1){
+            if (Index.index == 1) {
                 Index.index = 0
                 startActivity(Intent(this, BookingFormActivity::class.java))
-            }else{
+            } else {
                 Index.index = 1
                 startActivity(Intent(this, SRPActivity::class.java))
             }
 
-        }
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-
-        val identifier = intent?.getStringExtra("goto")
-        if (identifier != localIndex) {
-            startActivity(Intent(this, PDPActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-                putExtra("goto", "PDP 1")
-            })
         }
     }
 }
